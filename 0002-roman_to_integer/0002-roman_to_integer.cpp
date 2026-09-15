7cc173f204a4303f0bc34be37123c97d41ca5616
w1ldkidd <rasula216@gmail.com>
2026-09-15 11:28:16 +0500

    Update 0002-roman_to_integer.cpp

--- 0002-roman_to_integer/0002-roman_to_integer.cpp
+++ 0002-roman_to_integer/0002-roman_to_integer.cpp
@@ -3,7 +3,7 @@ public:
     int romanToInt(string s) {
         unordered_map<char, int> value = {
             {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-            {'C', 100}, {'D', '500'}, {'M', 1000}
+            {'C', 100}, {'D', 500}, {'M', 1000}
         };
 
         int n = s.size();
